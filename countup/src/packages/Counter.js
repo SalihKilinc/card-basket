2379725bcd313c58b960cc280999993ffd889daa
salih <salihkilinc80@gmail.com>
2023-05-14 12:26:57 +0200

    v6

--- countup/src/packages/Counter.js
+++ countup/src/packages/Counter.js
@@ -4,7 +4,7 @@ import CountUp, { useCountUp } from 'react-countup';
 
 function Counter  () {
     const countUpRef = React.useRef([null])
-    const { start, pauseResume, reset, update } = useCountUp({ref: countUpRef,duration: 5, start:0 , end:50})
+    const { start, pauseResume, reset, update } = useCountUp({ref: countUpRef,duration: 2, start:0 , end:50})
 
   return (
     <div className='count'> 
@@ -13,13 +13,13 @@ function Counter  () {
         
         <div className="box box1"><CountUp end={100} duration={2} delay={2}/></div>
         <div className="box box2" ref={countUpRef}>0</div>
-        <div className="box box3">80</div>
+        <div className="box box3">{<CountUp end={500} duration={3} prefix='&#8378;' suffix='CHF' />}</div>
          </div>
     <div className="buttons">
         <button onClick={start}>Start </button>
         <button onClick={pauseResume}>pauseResume </button>
         <button onClick={reset}>reset </button>
-        <button onClick={update}>update </button>
+        <button onClick={()=>update(120)}>update </button>
     </div>
     </div>
   )
