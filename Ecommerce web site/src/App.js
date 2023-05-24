a79d1ce23d3997717ca17bde4fa9699d9312a417
salih <salihkilinc80@gmail.com>
2023-05-24 17:46:31 +0200

    v3

--- Ecommerce web site/src/App.js
+++ Ecommerce web site/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/home/header/header';
 
 function App() {
   return (
-    <div className="App">
-      <p> selam </p>
-    </div>
+  <> 
+  <Header/>
+  </>
   );
 }
 
